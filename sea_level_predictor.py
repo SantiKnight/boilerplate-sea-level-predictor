79c11fa1f383c7bb6b3831cf95a86a978c5d1280
Santi Knight <santiknight2003@gmail.com>
2024-07-01 13:33:08 +0000

    Completed, little bug in plot's legend

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -3,21 +3,29 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
 def draw_plot():
-    # Read data from file
+    # Leer datos desde el archivo
+    df = pd.read_csv('epa-sea-level.csv')
 
+    # Crear un diagrama de dispersión
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
 
-    # Create scatter plot
+    # Crear la primera línea de mejor ajuste
+    slope, intercept, r_value, p_value, std_err = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    years_extended = pd.Series([i for i in range(1880, 2051)])
+    plt.plot(years_extended, intercept + slope * years_extended, 'r', label='Línea de mejor ajuste 1880-2050')
 
+    # Crear la segunda línea de mejor ajuste
+    df_recent = df[df['Year'] >= 2000]
+    slope_recent, intercept_recent, r_value_recent, p_value_recent, std_err_recent = linregress(df_recent['Year'], df_recent['CSIRO Adjusted Sea Level'])
+    years_recent_extended = pd.Series([i for i in range(2000, 2051)])
+    plt.plot(years_recent_extended, intercept_recent + slope_recent * years_recent_extended, 'g', label='Línea de mejor ajuste 2000-2050')
 
-    # Create first line of best fit
+    # Agregar etiquetas y título
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
+    plt.legend(loc='upper left')
 
-
-    # Create second line of best fit
-
-
-    # Add labels and title
-
-    
-    # Save plot and return data for testing (DO NOT MODIFY)
+    # Guardar gráfico y retornar datos para pruebas
     plt.savefig('sea_level_plot.png')
     return plt.gca()
